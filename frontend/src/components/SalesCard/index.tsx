222ad2b320e7cffeb1fbf95d093ee65f472e35fd
João Gilberto <jgsopro@gmail.com>
2022-11-19 11:49:33 -0300

    Sale listing

--- frontend/src/components/SalesCard/index.tsx
+++ frontend/src/components/SalesCard/index.tsx
@@ -2,6 +2,8 @@ import { useState, useEffect } from 'react'
 import axios from 'axios'
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
+import { Sale } from '../../models/sale'
+import { BASE_URL } from '../../utils/request'
 import NotificationButton from '../NotificationButton'
 
 import './styles.css'
@@ -13,9 +15,12 @@ function SalesCard() {
   const [minDate, setMinDate] = useState(min)
   const [maxDate, setMaxDate] = useState(max)
   
+  const [sales, setSales] = useState<Sale[]>([])
+  
   useEffect(() => {
-    axios.get('http://localhost:8080/sales').then(response => {
-      console.log(response.data)
+    axios.get(`${BASE_URL}/sales`).then(response => {
+      //console.log(response.data)
+      setSales(response.data.content)
     })
   }, [])
   
@@ -54,11 +59,30 @@ function SalesCard() {
             </tr>
           </thead>
           <tbody>
+            {
+              sales.map(sale => {
+                return (
+                  <tr key={sale.id}>
+                    <td className="show992">{sale.id}</td>
+                    <td className="show576">{new Date(sale.date).toLocaleDateString()}</td>
+                    <td>{sale.sellerName}</td>
+                    <td className="show992">{sale.visited}</td>
+                    <td className="show992">{sale.deals}</td>
+                    <td>R$ {sale.amount.toFixed(2)}</td>
+                    <td>
+                      <div className="dsmeta-red-btn-container">
+                        <NotificationButton />
+                      </div>
+                    </td>
+                  </tr>
+                )
+              })
+            }
             <tr>
               <td className="show992">#341</td>
               <td className="show576">08/07/2022</td>
               <td>Anakin</td>
-              <td className="show992">15</td>
+              <td className="show992">32</td>
               <td className="show992">11</td>
               <td>R$ 55300.00</td>
               <td>
@@ -71,7 +95,7 @@ function SalesCard() {
               <td className="show992">#341</td>
               <td className="show576">08/07/2022</td>
               <td>Anakin</td>
-              <td className="show992">15</td>
+              <td className="show992">25</td>
               <td className="show992">11</td>
               <td>R$ 55300.00</td>
               <td>
@@ -84,7 +108,7 @@ function SalesCard() {
               <td className="show992">#341</td>
               <td className="show576">08/07/2022</td>
               <td>Anakin</td>
-              <td className="show992">15</td>
+              <td className="show992">30</td>
               <td className="show992">11</td>
               <td>R$ 55300.00</td>
               <td>
