09f32dd256f2963a84c69a5986ff96795cc362bd
João Gilberto <jgsopro@gmail.com>
2022-11-20 22:04:19 -0300

    Twilio SMS

--- backend/src/main/java/com/devsuperior/dsmeta/repositories/SaleRepository.java
+++ backend/src/main/java/com/devsuperior/dsmeta/repositories/SaleRepository.java
@@ -9,6 +9,7 @@ import org.springframework.data.jpa.repository.Query;
 
 import com.devsuperior.dsmeta.entities.Sale;
 
+// Linguagem JPQL
 public interface SaleRepository extends JpaRepository<Sale, Long> {
 	
 	@Query("SELECT obj FROM Sale obj WHERE obj.date BETWEEN :min AND :max ORDER BY obj.amount DESC")
---
+++ backend/src/main/java/com/devsuperior/dsmeta/services/SmsService.java
@@ -0,0 +1,11 @@
+package com.devsuperior.dsmeta.services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class SmsService {
+	
+	public void sendSms(Long saleId) {
+		
+	}
+}
